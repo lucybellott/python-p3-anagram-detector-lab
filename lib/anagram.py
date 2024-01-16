24a20ea4f7fb3caac78fb2a21a91f4172f0d2f1f
Lucy <lucybellott@gmail.com>
2024-01-16 16:15:10 -0500

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def print_shit(self):
+        print(self.word_letters)    
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_word_list.append(word)
+
+        return match_word_list
+
+some_test = Anagram("sometest")
+
+some_test.print_shit()
